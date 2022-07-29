9e1125cbc699ca8ade36cb3c40e1267ed4a5fd54
Hrach <steepgaskin@gmail.com>
2022-07-29 13:17:35 +0400

    Meta update 2

--- src/components/seo.js
+++ src/components/seo.js
@@ -64,7 +64,7 @@ function Seo({ description, lang, meta, title }) {
         },
         {
           property: `og:image`,
-          content: `https://webly.pro/assets/share-image-min.png`
+          content: `https://webly-landing.netlify.app/assets/share-image-min.png`
         },
         {
           name: `twitter:creator`,
@@ -84,7 +84,7 @@ function Seo({ description, lang, meta, title }) {
         },
         {
           name: `twitter:image`,
-          content: `https://webly.pro/assets/webly-meta-preview.jpeg`
+          content: `https://webly-landing.netlify.app/assets/share-image-min.png`
         },
       ].concat(meta)}
     >
